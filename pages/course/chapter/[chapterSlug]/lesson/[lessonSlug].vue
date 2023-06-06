97e771b83f4bee9577f6610b4500b6d478fc9018
Felipe Amador <felipedavidamador@gmail.com>
2023-06-06 00:49:12 -0300

    `useHead`

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -40,4 +40,10 @@ const lesson = computed(() => {
     (lesson) => lesson.slug === route.params.lessonSlug
   );
 });
+
+const title = computed(() => `${lesson.value.title} - ${course.title}`);
+
+useHead({
+  title,
+});
 </script>
